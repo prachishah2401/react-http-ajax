77ead5cbc1a639a820591cf8062d4e2c6cf716c0
Prachi <prachishah2401@gmail.com>
2020-08-13 14:58:03 +1000

    rendering components for routes and switching between pages. Used routing-related props

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import Posts from  './Posts/Posts';
-
+import { Route, Link } from 'react-router-dom';
 import './Blog.css';
+import Posts from  './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
     render () {
@@ -12,12 +13,18 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><a href='/'>Home</a></li>
-                            <li><a href='/new-post'>New Post</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to={{
+                                pathname: '/new-post',
+                                hash: '#submit',
+                                search: '?quick-submit=true'
+                            }}>New Post</Link></li>
                         </ul>
                     </nav>
                 </header>
-                <Posts /> 
+                 {/* <Route path="/" exact render={() => <h1>Home</h1>}/> */}
+                 <Route path="/" exact component={Posts} />
+                 <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
