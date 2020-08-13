32eb9cd399a56620a91df5801ab4309bb592a800
Prachi <prachishah2401@gmail.com>
2020-08-13 16:00:45 +1000

    Style the active route. pass and extract route parameters. used swicth to load a single route

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import axios from '../../../axios';
+//import { Link } from 'react-router-dom';
 import Post from '../../../components/Post/Post';
 import './Posts.css';
 
@@ -30,18 +31,24 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({selectedPostID: id})
+        //this.setState({selectedPostID: id})
+        //this.props.history.push({pathname: '/' + id});
+        this.props.history.push('/' + id);
     }
 
     render () {
         let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
+                return (
+                  //  <Link to={'/' + post.id} key={post.id} >
+                        <Post 
+                            key={post.id}
+                            title={post.title} 
+                            author={post.author}
+                            clicked={() => this.postSelectedHandler(post.id)} />
+                   // </Link>
+                );
             });
         }
 
