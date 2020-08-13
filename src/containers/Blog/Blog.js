df62327b3da6744cb52def6bfc2f18ffbe0e6a3f
Prachi <prachishah2401@gmail.com>
2020-08-13 13:09:48 +1000

    set up router package and prepared the project for routing

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,65 +1,23 @@
 import React, { Component } from 'react';
 //import axios from 'axios';
-import axios from '../../axios';
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from  './Posts/Posts';
+
 import './Blog.css';
 
 class Blog extends Component {
-
-    state = {
-        posts : [],
-        selectedPostID : null,
-        error: false
-    }
-
-    componentDidMount () {
-        axios.get('/posts')
-            .then(response => {
-                const posts =response.data.slice(0, 4);
-                const updatedPosts = posts.map(post => {
-                    return {
-                        ...post,
-                        author: "Prachi"
-                    }
-                });
-            this.setState({posts: updatedPosts});
-            //console.log(response);
-        })
-        .catch(error => {
-            this.setState({error: true});
-            //console.log(error);
-        });  
-    }
-
-    postSelectedHandler = (id) => {
-        this.setState({selectedPostID: id})
-    }
-
     render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
-        if (!this.state.error) {
-            posts = this.state.posts.map(post => {
-                return <Post 
-                    key={post.id} 
-                    title={post.title} 
-                    author={post.author}
-                    clicked={() => this.postSelectedHandler(post.id)} />;
-            });
-        }
            
         return (
-            <div>
-                <section className="Posts">
-                    {posts}
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostID}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
+            <div className="Blog">
+                <header>
+                    <nav>
+                        <ul>
+                            <li><a href='/'>Home</a></li>
+                            <li><a href='/new-post'>New Post</a></li>
+                        </ul>
+                    </nav>
+                </header>
+                <Posts /> 
             </div>
         );
     }
